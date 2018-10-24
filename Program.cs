1c1fdfee9f8732df9bdf3e18c7685bc2841b763b
student <student@itlab.pmmk.pte.hu>
2018-10-24 08:30:56 +0200

    Exercise 2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, divisor = 5;
+            Console.Write("Enter the number: ");
+            if (int.TryParse(Console.ReadLine(),out number)&& number > 0 )
+            {
+            //number = int.Parse(Console.ReadLine());
+            if (number%2==0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+            if (number%divisor==0)
+            {
+                Console.WriteLine("Divisible by " + divisor);
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by " + divisor);
+            }
+        }
+            else
+            {
+                Console.WriteLine("Wrong data !");
+            }
+    }
+    }
+
+}
+
